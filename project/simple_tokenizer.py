7276c18222c5bfe3a37875627d7a95797b705629
Justin Reppert <jreppert@gmail.com>
2021-01-23 08:48:53 -0500

    write simple tokenizer tests

---
+++ project/simple_tokenizer.py
@@ -0,0 +1,124 @@
+import os
+import pickle
+from collections import Counter
+from itertools import count
+from pathlib import Path
+from typing import List, Union
+
+
+class WordTokenizer:
+    """Implements a simple, pure-python, word-based tokenizer, with some of the
+    same API as tokenizers.BertWordPieceTokenizer."""
+
+    def __init__(self) -> None:
+        self._incrementer = count()
+        self.tokens = dict()
+        self.padding = False
+        self.truncation = False
+        self.is_trained = False
+        
+    def check_trained(self):
+        if not self.is_trained:
+            raise ValueError('Train tokenizer before using it')
+
+    def train(
+        self,
+        filename: Union[str, Path],
+        vocab_size: int,
+        min_frequency: int,
+        special_tokens: List[int],
+        initial_alphabet: str = None,
+        limit_alphabet: int = None,
+    ):
+        """Train the word-based tokenizer from a file.
+
+        Arguments:
+            filename {Union[str, Path]} -- Path to training data with strings separated by newlines,
+            with words split on whitespace. Does not strip punctuation or change case.
+            vocab_size {int} -- Use at most this many words
+            min_frequency {int} -- Only include words used at least this many times
+            special_tokens {List[int]} -- Special tokens in order of PAD, BOS, EOS, UNK
+
+        Keyword Arguments:
+            initial_alphabet {str} -- Ignored; in for API compatibility (default: {None})
+            limit_alphabet {int} -- Ignored; in for API compatibility (default: {None})
+        """
+        if self.is_trained:
+            raise ValueError("Don't retrain; use a new instance instead")
+        self.max_vocab_size = vocab_size
+        self.min_frequency = min_frequency
+        
+        if len(special_tokens) != 4:
+            raise ValueError('Special tokens must have exactly 4 values: PAD, BOS, EOS, UNK')
+        
+        self.special_tokens = special_tokens
+
+        for t in self.special_tokens:
+            self.tokens[t] = next(self._incrementer)
+
+        with open(filename, "r") as f:
+            data = f.readlines()
+        data = map(lambda s: s.lower().split(), data)
+        counter = Counter()
+        list(map(lambda lst: counter.update(lst), data))
+        vocab = counter.most_common(self.max_vocab_size - len(self.tokens))
+        vocab = filter(lambda tup: tup[1] >= self.min_frequency, vocab)
+        for word, _ in vocab:
+            self.tokens[word] = next(self._incrementer)
+        self.ids = {v: k for k, v in self.tokens.items()}
+        self.is_trained = True
+
+    def token_to_id(self, token):
+        self.check_trained()
+        return self.tokens.get(token)
+
+    @property
+    def vocab_size(self):
+        return len(self.tokens)
+
+    def get_vocab_size(self):
+        return self.vocab_size
+
+    def decode_batch(self, batch: List[List[int]], skip_special_tokens=True):
+        return list(self._decode_batch(batch, skip_special_tokens))
+
+    def _decode_batch(self, batch: List[List[int]], skip_special_tokens):
+        self.check_trained()
+        for element in batch:
+            element = map(lambda i: self.ids.get(i, ""), element)
+            if skip_special_tokens:
+                element = filter(lambda w: w not in self.special_tokens, element)
+            yield " ".join(element)
+
+    def encode_batch(self, batch: List[str]):
+        return list(self._encode_batch(batch))
+
+    def _encode_batch(self, batch: List[str]):
+        self.check_trained()
+        for element in batch:
+            split = element.strip().split(" ")
+            if self.truncation and len(split) > self.max_length:
+                split = split[: self.max_length]
+            if self.padding and len(split) < self.max_length:
+                split = split + [self.special_tokens[0]] * (self.max_length - len(split))
+            yield list(map(lambda w: self.tokens.get(w, 3), split))
+
+    def save_model(self, directory, filename):
+        self.check_trained()
+        with open(os.path.join(directory, filename), "wb") as f:
+            pickle.dump(self.__dict__, f, 3)
+
+    def load_model(self, directory, filename):
+        with open(os.path.join(directory, filename), "rb") as f:
+            tmp_dict = pickle.load(f)
+        self.__dict__.update(tmp_dict)
+
+    def enable_padding(self):
+        if not self.truncation:
+            raise AttributeError("Enable truncation before enabling padding")
+        self.padding = {"pad_id": 0}
+
+    def enable_truncation(self, max_length):
+        self.truncation = True
+        self.max_length = max_length
+
---
+++ tests/test_simple_tokenizer.py
@@ -0,0 +1,83 @@
+import os
+
+import pytest
+from project.simple_tokenizer import WordTokenizer
+
+
+@pytest.fixture
+def trained_word_tokenizer(tmpdir):
+    input = """this is a sentence\nthis is but another sentence\nthis is a third sentence
+    \nthis is really long truncation not enabled so will it perfectly convert back
+    """
+    d = tmpdir.mkdir("test_tokenizer")
+    filename = os.path.join(d, "input.txt")
+    with open(filename, "w") as f:
+        f.writelines(input)
+    wt = WordTokenizer()
+    wt.train(filename, 30, 1, ["[PAD]", "[BOS]", "[EOS]", "[UNK]"], None, None)
+    return wt
+
+
+def test_simple_tokenizer(trained_word_tokenizer):
+    wt = trained_word_tokenizer
+
+    assert wt.token_to_id("[PAD]") == 0
+    assert wt.token_to_id("[BOS]") == 1
+    assert wt.token_to_id("[EOS]") == 2
+    assert wt.token_to_id("[UNK]") == 3
+
+    assert wt.vocab_size == 22
+
+    test_strs = [
+        "this is a sentence",
+        "this another sentence is",
+        "this is really really long but truncation is not enabled so it will convert back perfectly",
+        "sentence is",
+    ]
+    encoded = wt.encode_batch(test_strs)
+    decoded = wt.decode_batch(encoded)
+    assert decoded == test_strs
+
+
+def test_simple_tokenizer_truncation(trained_word_tokenizer):
+    wt = trained_word_tokenizer
+    wt.enable_truncation(max_length=25)
+
+    encoded = wt.encode_batch(["perfectly " * 50])
+
+    assert len(encoded[0]) == 25
+
+
+def test_simple_tokenizer_padding(trained_word_tokenizer):
+    wt = trained_word_tokenizer
+    wt.enable_truncation(max_length=10)
+    wt.enable_padding()
+
+    encoded = wt.encode_batch(["is " * 3, "will " * 10, "unknown " * 30])
+
+    for element in encoded:
+        assert len(element) == 10
+
+    assert encoded[0][3] == 0
+    assert encoded[1][-1] != 0
+    assert encoded[2][-1] == 3
+
+
+def test_simple_tokenizer_save_load(tmpdir, trained_word_tokenizer):
+    wt = trained_word_tokenizer
+    d = tmpdir.mkdir("test_save_tokenizer")
+
+    wt.enable_truncation(30)
+    wt.enable_padding()
+    wt.save_model(d, "saved_tokenizer.pkl")
+
+    wt2 = WordTokenizer()
+    wt2.load_model(d, "saved_tokenizer.pkl")
+
+    assert wt.tokens == wt2.tokens
+    assert wt.padding == wt2.padding
+    assert wt.vocab_size == wt2.vocab_size
+
+    encoded = wt.encode_batch(["sample sentence to encode"])
+    encoded2 = wt2.encode_batch(["sample sentence to encode"])
+    assert encoded == encoded2
